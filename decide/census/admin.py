306d5405226d877b5f9d0ed3dee5b3d8aa0297f8
josreimun <josreimun@alum.us.es>
2023-11-28 18:37:07 +0100

    Feat: Implement census export
    First commit for pair-programming to implement census export(Not finished)
    
    References #3

--- decide/census/admin.py
+++ decide/census/admin.py
@@ -1,7 +1,24 @@
+import csv
+import datetime
+from django.http import HttpResponse
+
 from django.contrib import admin
 
 from .models import Census
 
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not \
+                field.many_to_many and not field.one_to_many]
+    #Escribe una primera fila con la "header" informacion
+    writer.writerow([field.verbose_name for field in fields])
+    #Escribe data rows
+    for obj in queryset:
+        data_row = []
 
 class CensusAdmin(admin.ModelAdmin):
     list_display = ('voting_id', 'voter_id')

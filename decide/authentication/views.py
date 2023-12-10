f2800d89f4cdc2190f55d077d039e6d23013df6a
ldrakohh <ldrakohh@gmail.com>
2023-12-10 14:23:10 +0100

    feat: Added send_email when signing up.

--- decide/authentication/views.py
+++ decide/authentication/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login 
 from .forms import LoginForm, UserRegistrationForm
+from django.core.mail import send_mail
+from django.conf import settings
 
 def user_login(request):
     if request.method == 'POST':
@@ -38,9 +40,19 @@ def register(request):
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
             new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.cleaned_data['password1']) 
+            new_user.set_password(user_form.cleaned_data['password1'])
             new_user.save()
             print("Usuario guardado exitosamente:", new_user.username)
+            
+            # Envío del correo electrónico
+            subject = 'Confirmar cuenta'
+            message = 'Te has registrado correctamente. ¡Gracias por registrarte!'
+            from_email = 'tu_email@tu_dominio.com'  # Reemplaza con tu dirección de correo electrónico
+
+            # Envía el correo al correo electrónico proporcionado en el formulario de registro
+            to_email = new_user.email
+            send_mail(subject, message, settings.EMAIL_HOST_USER, [to_email], fail_silently=False)
+
             return render(request, 'register_done.html', {'new_user': new_user})
         else:
             print("Errores en el formulario:", user_form.errors)
@@ -48,3 +60,4 @@ def register(request):
         user_form = UserRegistrationForm()
     
     return render(request, 'register.html', {'user_form': user_form})
+

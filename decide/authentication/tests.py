91ebdf32a863275bb5a92c528d7784ba00399b32
pabparmen <pabparmen@alum.us.es>
2023-12-15 00:42:29 +0100

    Test: add test from UserRegistrationForm

--- decide/authentication/tests.py
+++ decide/authentication/tests.py
@@ -1,10 +1,8 @@
-from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework.test import APITestCase
-
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
-
+from .forms import UserRegistrationForm
 from base import mods
 
 
@@ -13,9 +11,9 @@ class AuthTestCase(APITestCase):
     def setUp(self):
         self.client = APIClient()
         mods.mock_query(self.client)
-        u = User(username='voter1')
-        u.set_password('123')
-        u.save()
+        self.user_voter1 = User.objects.create(username='voter1')
+        self.user_voter1.set_password('123')
+        self.user_voter1.save()
 
         u2 = User(username='admin')
         u2.set_password('admin')
@@ -45,7 +43,7 @@ class AuthTestCase(APITestCase):
         self.assertEqual(response.status_code, 200)
 
         user = response.json()
-        self.assertEqual(user['id'], 1)
+        self.assertEqual(user['id'], self.user_voter1.id)
         self.assertEqual(user['username'], 'voter1')
 
     def test_getuser_invented_token(self):
@@ -121,3 +119,29 @@ class AuthTestCase(APITestCase):
             sorted(list(response.json().keys())),
             ['token', 'user_pk']
         )
+
+
+    def test_clean_password2_matching_passwords(self):
+        form = UserRegistrationForm(data={'username': 'test123', 'password': '123', 'password2': '123'})
+        self.assertTrue(form.is_valid())
+        cleaned_password = form.clean_password2()
+        self.assertEqual(cleaned_password, '123')
+
+    def test_save_user_active(self):
+            data = {'username': 'admin', 'password': 'admin'}
+            response = self.client.post('/authentication/login/', data, format='json')
+            self.assertEqual(response.status_code, 200)
+            token = response.json()
+
+            token.update({'username': 'user45', 'password': 'pwd1'})
+            response = self.client.post('/authentication/register/', token, format='json')
+            self.assertEqual(response.status_code, 201)
+            self.assertEqual(
+                sorted(list(response.json().keys())),
+                ['token', 'user_pk']
+            )
+            
+            saved_user = User.objects.get(username='user45')
+            self.assertEqual(saved_user.username, 'user45')
+            self.assertTrue(saved_user.is_active) 
+            self.assertTrue(saved_user.check_password('pwd1'))

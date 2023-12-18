5589d2a36a8fc33813c3bdeb35d7b48590e7d346
diegonqui <digonquin@gmail.com>
2023-12-18 12:49:58 +0100

    Refactor: Does minor refactor changes
    Does minor refactor changes to solve issues on Codacy (whitespaces)

--- decide/census/models.py
+++ decide/census/models.py
@@ -5,14 +5,11 @@ import re
 class Census(models.Model):
     voting_id = models.PositiveIntegerField()
     voter_id = models.PositiveIntegerField()
-
     def get_all_objects(self): 
         queryset=self._meta.model.objects.all() 
         return queryset
-
     class Meta:
         unique_together = (('voting_id', 'voter_id'),)
-
 
 class LdapCensus:
     def ldapConnection(self, urlServer, auth, psw):

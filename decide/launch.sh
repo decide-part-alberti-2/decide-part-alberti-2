0638e01234ce0e2c461acd49af3e35646ff93643
diegonqui <digonquin@gmail.com>
2023-12-18 14:08:58 +0100

    Chore: Checks if a superuser is created
    This commit checks if a superuser is created using the env variables on Render

--- decide/launch.sh
+++ decide/launch.sh
@@ -6,3 +6,5 @@ cp local_settings.deploy.py local_settings.py
 ./manage.py makemigrations
 ./manage.py migrate
 gunicorn -w 5 decide.wsgi:application --timeout=500
+echo "DEBUG: DJANGO_SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME}"
+echo "DEBUG: DJANGO_SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD}"
